168c3a74993ed3b4661e3b89e678b129073e64cf
Lavanya Vemula <lavanyavani266@gmail.com>
2024-07-27 11:42:50 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import TransactionList from './components/TransactionList'
+import AddTransaction from "./components/AddTranscation";
 
 function App(){
   return(
     <BrowserRouter>
         <Routes>
           <Route path="/" exact Component={TransactionList} />
+          <Route path="/add" Component={AddTransaction} />
         </Routes>
     </BrowserRouter>  
 )
